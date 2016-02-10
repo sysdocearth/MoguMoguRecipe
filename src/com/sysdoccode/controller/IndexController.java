e0b97b6705fe945d5c7f1544147eb53bcfaa8920
Takeuchi <Takeuchi@Takeuchi-PC>
2016-02-10 12:32:37 +0900

    Add Comment

--- src/com/sysdoccode/controller/IndexController.java
+++ src/com/sysdoccode/controller/IndexController.java
@@ -3,10 +3,17 @@ package com.sysdoccode.controller;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
+/**
+ * URL トップページにアクセスしたときのクラスです。
+ * 
+ * @author Y.Takeuchi
+ * @version　1.0.0
+ */
 public class IndexController extends Controller {
 
     @Override
     public Navigation run() throws Exception {
+        //リダイレクトします
         return redirect("/recipe/");
     }
 }
